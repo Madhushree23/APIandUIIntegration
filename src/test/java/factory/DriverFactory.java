d89187b1ecc150412d425f0bbdc5bd375de6cb39
Madhushree23 <madhushree91102@gmail.com>
2026-04-28 20:08:52 +0530

    trigger test

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -2,7 +2,7 @@ package factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//this is factory
+//this is factory  class
 public class DriverFactory {
 	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
